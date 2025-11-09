e88f9684eda6bd641da4ec99e84d325729037c2d
Bagasszz <bagasadhinugraha6@gmail.com>
2025-11-09 21:50:05 +0700

    fix admin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#ec4899', // Pink untuk buket
-        secondary: '#8b5cf6', // Purple
+        primary: "#ec4899", // Pink untuk buket
+        secondary: "#8b5cf6", // Purple
       },
     },
   },
   plugins: [],
-}
+};
